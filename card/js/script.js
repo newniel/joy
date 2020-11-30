84a7e1fac121d3f8bd4c2ae09b93a89bb77a8993
endlessiris <endlessiris@naver.com>
2020-11-30 13:42:26 +0900

    no message

--- card/js/script.js
+++ card/js/script.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
 
   var $clickMe = $('.click-icon'),
-      $card = $('.card');
-  
+      $card = $('.card'),
+      $audioFile = new Audio('./mp3/christmas.mp3');
+
+      
   $card.on('click', function() {
 		$(this).toggleClass('is-opened');
     $clickMe.toggleClass('is-hidden');
-
+    var played = $(this).hasClass('is-opened');
+    if(played){
+      $audioFile.play();
+    }else{
+      $audioFile.pause();
+      $audioFile.currentTime = 0;
+    }
 	});
 
 });
